c2d9e81170d93fec77a989ace85eae3be991e027
MurilloFornazzaCosta <mfornazzamurilloo@gmail.com>
2024-08-28 16:50:38 -0300

    front dos graficos prontos, aguardando os dados

--- js/mainChart.js
+++ js/mainChart.js
@@ -14,12 +14,12 @@ document.addEventListener('DOMContentLoaded', function(){
                 label: 'Relatório de vendas',
                 data: [12, 19, 3, 5, 2, 3, 5, 20, 12, 24, 5, 12], // Você pode ajustar os dados aqui conforme necessário
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 99, 132, 0.7)',
+                    'rgba(54, 162, 235, 0.7)',
+                    'rgba(255, 206, 86, 0.7)',
+                    'rgba(75, 192, 192, 0.7)',
+                    'rgba(153, 102, 255, 0.7)',
+                    'rgba(255, 159, 64, 0.7)'
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
